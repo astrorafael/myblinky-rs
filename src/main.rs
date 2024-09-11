a2d01fdae679b271157eb727fa9b489467019894
Rafael Gonzalez <astrorafael@yahoo.es>
2024-09-11 12:26:29 +0200

    added tasks

--- src/main.rs
+++ src/main.rs
@@ -11,23 +11,32 @@ use {defmt_rtt as _, panic_probe as _};
 
 use embassy_executor::Spawner;
 use embassy_nrf::gpio::{Level, Output, OutputDrive};
-use embassy_time::Timer;
-use fmt::{debug, info};
+use embassy_time::{Duration, Timer};
+use fmt::info;
 
+#[embassy_executor::task(pool_size = 5)]
+async fn blinker(mut led: Output<'static>, interval: Duration, i: i32) {
+    loop {
+        info!("Task {}", i);
+        led.set_high();
+        Timer::after(interval).await;
+        led.set_low();
+        Timer::after(interval).await;
+    }
+}
 #[embassy_executor::main]
-async fn main(_spawner: Spawner) {
+async fn main(spawner: Spawner) {
     let p = embassy_nrf::init(Default::default());
     let _row1 = Output::new(p.P0_21, Level::High, OutputDrive::Standard);
-    let mut col1 = Output::new(p.P0_28, Level::High, OutputDrive::Standard);
-    let mut col2 = Output::new(p.P0_11, Level::Low, OutputDrive::Standard);
-    info!("Hello world");
-    loop {
-        debug!("Looping...");
-        col2.set_high();
-        col1.set_low();
-        Timer::after_millis(300).await;
-        col2.set_low();
-        col1.set_high();
-        Timer::after_millis(300).await;
-    }
+    let col1 = Output::new(p.P0_28, Level::High, OutputDrive::Standard);
+    /*let col2 = Output::new(p.P0_11, Level::Low, OutputDrive::Standard);
+    let col3 = Output::new(p.P0_31, Level::Low, OutputDrive::Standard);
+    let col4 = Output::new(p.P1_05, Level::Low, OutputDrive::Standard);
+    let col5 = Output::new(p.P0_30, Level::Low, OutputDrive::Standard);*/
+    info!("Starting LED tasks");
+    crate::fmt::unwrap!(spawner.spawn(blinker(col1, Duration::from_millis(900), 1)));
+    /*crate::fmt::unwrap!(spawner.spawn(blinker(col2, Duration::from_millis(900), 2)));
+    crate::fmt::unwrap!(spawner.spawn(blinker(col3, Duration::from_millis(900), 3)));
+    crate::fmt::unwrap!(spawner.spawn(blinker(col4, Duration::from_millis(900), 4)));
+    crate::fmt::unwrap!(spawner.spawn(blinker(col5, Duration::from_millis(900), 5)));*/
 }
