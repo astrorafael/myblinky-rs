ba3dda3f986dd8ad5209968ee5aa148d33d153f9
Rafael Gonzalez <astrorafael@yahoo.es>
2024-09-18 12:04:47 +0200

    Refactored to include a hardware setup function

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,10 @@ use panic_halt as _;
 use {defmt_rtt as _, panic_probe as _};
 
 use embassy_executor::Spawner;
-use embassy_nrf::gpio::{Input, Level, Output, OutputDrive, Pull};
+use embassy_nrf::{
+    gpio::{Input, Level, Output, OutputDrive, Pull},
+    Peripherals,
+};
 use embassy_time::{Duration, Timer};
 use fmt::{info, unwrap};
 
@@ -33,29 +36,49 @@ async fn button_task(mut pin: Input<'static>, tag: char) {
     }
 }
 
+// Micro:bit hardware resources to use
+pub struct HwResources {
+    _row1: embassy_nrf::gpio::Output<'static>,
+    col1: embassy_nrf::gpio::Output<'static>,
+    col2: embassy_nrf::gpio::Output<'static>,
+    col3: embassy_nrf::gpio::Output<'static>,
+    col4: embassy_nrf::gpio::Output<'static>,
+    col5: embassy_nrf::gpio::Output<'static>,
+    btn_a: embassy_nrf::gpio::Input<'static>,
+    btn_b: embassy_nrf::gpio::Input<'static>,
+}
+
+impl HwResources {
+    fn new(p: Peripherals) -> Self {
+        Self {
+            // LED Matrix, columns 1 to 5
+            col1: Output::new(p.P0_28, Level::Low, OutputDrive::Standard),
+            col2: Output::new(p.P0_11, Level::Low, OutputDrive::Standard),
+            col3: Output::new(p.P0_31, Level::Low, OutputDrive::Standard),
+            col4: Output::new(p.P1_05, Level::Low, OutputDrive::Standard),
+            col5: Output::new(p.P0_30, Level::Low, OutputDrive::Standard),
+            // LED Matrix Row 1
+            _row1: Output::new(p.P0_21, Level::High, OutputDrive::Standard),
+            // Push Buttons
+            btn_a: Input::new(p.P0_14, Pull::None),
+            btn_b: Input::new(p.P0_23, Pull::None),
+        }
+    }
+}
+
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
     let p = embassy_nrf::init(Default::default());
-    let _row1 = Output::new(p.P0_21, Level::High, OutputDrive::Standard);
-
-    let col1 = Output::new(p.P0_28, Level::Low, OutputDrive::Standard);
-    let col2 = Output::new(p.P0_11, Level::Low, OutputDrive::Standard);
-    let col3 = Output::new(p.P0_31, Level::Low, OutputDrive::Standard);
-    let col4 = Output::new(p.P1_05, Level::Low, OutputDrive::Standard);
-    let col5 = Output::new(p.P0_30, Level::Low, OutputDrive::Standard);
-
-    // These two are externally pulled up by 10k resistors (R4 & R5)
-    let btn_a = Input::new(p.P0_14, Pull::None);
-    let btn_b = Input::new(p.P0_23, Pull::None);
+    let hw = HwResources::new(p);
 
     info!("Starting LED tasks");
-    unwrap!(spawner.spawn(blinker(col1, Duration::from_millis(900), 1)));
-    unwrap!(spawner.spawn(blinker(col2, Duration::from_millis(800), 2)));
-    unwrap!(spawner.spawn(blinker(col3, Duration::from_millis(700), 3)));
-    unwrap!(spawner.spawn(blinker(col4, Duration::from_millis(600), 4)));
-    unwrap!(spawner.spawn(blinker(col5, Duration::from_millis(500), 5)));
-    unwrap!(spawner.spawn(button_task(btn_a, 'A')));
-    unwrap!(spawner.spawn(button_task(btn_b, 'B')));
+    unwrap!(spawner.spawn(blinker(hw.col1, Duration::from_millis(900), 1)));
+    unwrap!(spawner.spawn(blinker(hw.col2, Duration::from_millis(800), 2)));
+    unwrap!(spawner.spawn(blinker(hw.col3, Duration::from_millis(700), 3)));
+    unwrap!(spawner.spawn(blinker(hw.col4, Duration::from_millis(600), 4)));
+    unwrap!(spawner.spawn(blinker(hw.col5, Duration::from_millis(500), 5)));
+    unwrap!(spawner.spawn(button_task(hw.btn_a, 'A')));
+    unwrap!(spawner.spawn(button_task(hw.btn_b, 'B')));
 
     loop {
         Timer::after_millis(1000).await;
